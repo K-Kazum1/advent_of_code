9aef4dc27e3e64435e09e861ccfd720a7a913227
K-Kazum1 <mitchwiig@gmail.com>
2021-12-07 14:15:31 +0900

    day 7

---
+++ day7/day7-2.py
@@ -0,0 +1,5 @@
+import numpy as np
+f = open('input','r')
+a=np.array([int(i) for i in f.readline().split(',')])
+
+print(min([(lambda x:np.abs(x*(np.abs(x)+1)/2))(a-i).sum() for i in range(a.min(),a.max()+1)]))
---
+++ day7/day7.py
@@ -0,0 +1,5 @@
+import numpy as np
+f = open('input','r')
+a=np.array([int(i) for i in f.readline().split(',')])
+
+print(min([abs(a-i).sum() for i in range(a.min(),a.max()+1)]))
